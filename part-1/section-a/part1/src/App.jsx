2e92da6d504b68c8dee34994e5b49722df410d9b
Tomás R <tomaslopezraigal@gmail.com>
2024-09-10 13:19:32 +0200

    Using props in the components

--- part-1/section-a/part1/src/App.jsx
+++ part-1/section-a/part1/src/App.jsx
@@ -1,15 +1,19 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Tomás"/>
+      <Hello name="Sombo"/>
+      <Hello name="ErMati"/>
     </div>
   )
 }
