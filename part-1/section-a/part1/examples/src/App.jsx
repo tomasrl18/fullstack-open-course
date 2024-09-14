a394b8f55d15d88dff5e569f7471576350aa774b
Tomás R <tomaslopezraigal@gmail.com>
2024-09-14 19:21:20 +0200

    Example section d: Conditional rendering

--- part-1/section-a/part1/examples/src/App.jsx
+++ part-1/section-a/part1/examples/src/App.jsx
@@ -1,35 +1,51 @@
 import { useState } from 'react'
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      Button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
     const updatedLeft = left + 1
     setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     const updatedRight = right + 1
     setRight(updatedRight)
-    setTotal(updatedRight + left)
   }
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>Left</button>
-      |
-      <button onClick={handleRightClick}>Light</button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
       {right}
-
-      <p>{allClicks.join(' ')}</p>
-      <p>Total: {total}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
