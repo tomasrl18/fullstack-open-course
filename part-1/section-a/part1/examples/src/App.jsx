70e10fd471542aba3575d2c078942dea32cd6c59
Tomás R <tomaslopezraigal@gmail.com>
2024-09-14 19:39:49 +0200

    Example section d: Review of event controllers

--- part-1/section-a/part1/examples/src/App.jsx
+++ part-1/section-a/part1/examples/src/App.jsx
@@ -1,54 +1,17 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        The app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      Button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
 const App = () => {
-  // estos están bien
-  const [age, setAge] = useState(0)
-  const [name, setName] = useState('Juha Tauriainen')
+  const [value, setValue] = useState(10)
 
-  if ( age > 10 ) {
-    // esto no funciona!
-    const [foobar, setFoobar] = useState(null)
-  }
-
-  for ( let i = 0; i < age; i++ ) {
-    // esto tampoco está bien
-    const [rightWay, setRightWay] = useState(false)
-  }
-
-  const notGood = () => {
-    // y esto también es ilegal
-    const [x, setX] = useState(-1000)
+  const handleClick = () => {
+    console.log('clicked the button')
+    setValue(0)
   }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={handleClick}>button</button>
     </div>
   )
 }
