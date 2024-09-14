d02a5ca012f9587f84ec77028247ba12b4fd242f
Tomás R <tomaslopezraigal@gmail.com>
2024-09-14 18:27:12 +0200

    Example section c: Example of re-rendering

--- part-1/section-a/part1/examples/src/App.jsx
+++ part-1/section-a/part1/examples/src/App.jsx
@@ -1,26 +1,8 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-
+const App = (props) => {
+  const {counter} = props
+  
   return (
-    <div>
-
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-      <br />
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
