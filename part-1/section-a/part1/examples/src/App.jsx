8eed0b8d63232ca6b1de0e30daf469a58cf02841
Tomás R <tomaslopezraigal@gmail.com>
2024-09-14 19:17:41 +0200

    Example section d: Arrays handling and The status update is asynchronous

--- part-1/section-a/part1/examples/src/App.jsx
+++ part-1/section-a/part1/examples/src/App.jsx
@@ -1,22 +1,35 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const handleLeftClick = () =>
-    setClicks({ ...clicks, left: clicks.left + 1 })
-  
-  const handleRightClick = () =>
-    setClicks({ ...clicks, right: clicks.right + 1 })
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight + left)
+  }
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {left}
+      <button onClick={handleLeftClick}>Left</button>
+      |
+      <button onClick={handleRightClick}>Light</button>
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
+      <p>Total: {total}</p>
     </div>
   )
 }
