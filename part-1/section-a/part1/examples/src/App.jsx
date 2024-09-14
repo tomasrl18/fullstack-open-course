886846c4e354a72ea13201979eeee42383180dad
Tomás R <tomaslopezraigal@gmail.com>
2024-09-14 18:43:44 +0200

    Example section c: Passing the state to a child

--- part-1/section-a/part1/examples/src/App.jsx
+++ part-1/section-a/part1/examples/src/App.jsx
@@ -1,22 +1,42 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
+      <Display counter={counter}/>
 
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
