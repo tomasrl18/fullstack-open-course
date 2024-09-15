76d9341b1c704e5e588063f5d6860420693b4048
Tomás R <tomaslopezraigal@gmail.com>
2024-09-15 21:32:33 +0200

    Done exercise 1.8: unicafe step 3

--- part-1/section-a/part1/unicafe/src/App.jsx
+++ part-1/section-a/part1/unicafe/src/App.jsx
@@ -4,35 +4,25 @@ const CustomTitle = ({ text }) => {
   return <h1>{text}</h1>
 }
 
-const Statistics = ({ feedback, text, feedbackType }) => {
-  switch (feedbackType) {
-    case 'good':
-      return (
-        <>
-          <span>{text} {feedback.good}</span>
-          <br />
-        </>
-      )
-    case 'neutral':
-      return (
-        <>
-          <span>{text} {feedback.neutral}</span>
-          <br />
-        </>
-      )
-    case 'bad':
-      return (
-        <>
-          <span>{text} {feedback.bad}</span>
-        </>
-      )
-  }
-}
+const AllStatistics = (props) => {
+  console.log(props);
 
-const MoreStatistic = ({ text, calc }) => {
   return (
     <>
-      <span>{text} {calc}</span>
+      <span>Good feedback: {props.feedback.good}</span>
+      <br />
+      <span>Neutral feedback: {props.feedback.neutral}</span>
+      <br />
+      <span>Bad feedback: {props.feedback.bad}</span>
+
+      <br />
+      <br />
+
+      <span>Total comments: {props.totalComments}</span>
+      <br />
+      <span>Average comments: {props.avgComments}</span>
+      <br />
+      <span>Positive comments: {props.positiveComments}</span>
     </>
   )
 }
@@ -72,20 +62,14 @@ const App = () => {
       <br />
       <br />
 
-      <CustomTitle text="Statistics" />
+      <CustomTitle text="Statistics"/>
 
-      <Statistics text="Good feedback:" feedback={feedback} feedbackType="good" />
-      <Statistics text="Neutral feedback:" feedback={feedback} feedbackType="neutral" />
-      <Statistics text="Bad feedback:" feedback={feedback} feedbackType="bad" />
-
-      <br />
-      <br />
-
-      <MoreStatistic text="All comments:" calc={totalComments()}/>
-      <br />
-      <MoreStatistic text="Average comments:" calc={avgComments()}/>
-      <br />
-      <MoreStatistic text="Positive comments:" calc={feedback.good}/>
+      <AllStatistics
+        feedback={feedback}
+        totalComments={totalComments()}
+        avgComments={avgComments()}
+        positiveComments={feedback.good}
+      />
     </>
   )
 }
