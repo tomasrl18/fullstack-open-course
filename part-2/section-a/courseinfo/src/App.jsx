1bf91c7917666fce2ece818ed654b93e6f897d76
Tomás R <tomaslopezraigal@gmail.com>
2024-09-17 13:09:50 +0200

    Done exercise 2.2: courseinfo step 6

---
+++ part-2/section-a/courseinfo/src/App.jsx
@@ -0,0 +1,42 @@
+const Course = ({ course }) => {
+  let parts = course.parts
+  
+  return (
+    <>
+      <h1>{course.name}</h1>
+      {parts.map(part => 
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      )}
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
