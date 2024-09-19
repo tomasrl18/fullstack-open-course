70e1e1cda1acaa88247c46fd4d2e9b6dbd8fa6dd
Tomás R <tomaslopezraigal@gmail.com>
2024-09-19 18:19:32 +0200

    Example section c: Axios and promises

--- part-2/section-c/examples/src/main.jsx
+++ part-2/section-c/examples/src/main.jsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client"
 
+import axios from 'axios'
+
 import App from "./App"
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
