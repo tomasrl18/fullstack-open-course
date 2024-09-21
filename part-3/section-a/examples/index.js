88f4835eb4e5c47303682fc94b23c837d92c61cf
Tomás R <tomaslopezraigal@gmail.com>
2024-09-21 12:13:05 +0200

    Delete resources

--- part-3/section-a/examples/index.js
+++ part-3/section-a/examples/index.js
@@ -37,6 +37,13 @@ app.get('/api/notes/:id', (request, response) => {
         response.status(404).end()
     }
 })
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+})
   
 const PORT = 3001
 app.listen(PORT, () => {
