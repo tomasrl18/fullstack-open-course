e4e5c0e49ba6852b634ac560d8363b77b6286b8f
Tomás R <tomaslopezraigal@gmail.com>
2024-09-23 20:52:07 +0200

    Using the db in the route controllers

--- part-3/section-c/examples/index.js
+++ part-3/section-c/examples/index.js
@@ -55,33 +55,25 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({ 
-        error: 'content missing' 
-        })
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
-
-    const note = {
-        content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId(),
-    }
-
-    notes = notes.concat(note)
-
-    response.json(note)
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
