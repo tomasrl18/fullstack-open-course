6a216580b4e2c1d8cdce0afe6988a1433227c54e
Tomás R <tomaslopezraigal@gmail.com>
2024-09-25 21:01:08 +0200

    Some fixes to deploy with db

--- part-3/section-c/examples/index.js
+++ part-3/section-c/examples/index.js
@@ -14,11 +14,11 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
+const cors = require('cors')
+
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-
-const cors = require('cors')
 
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
