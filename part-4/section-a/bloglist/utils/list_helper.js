c1a1df93df04035ead80102b9df1febca8c6f1f3
Tomás R <tomaslopezraigal@gmail.com>
2024-09-30 22:31:12 +0200

    Done exercise 4.4: Auxiliar functions and unit tests step2

--- part-4/section-a/bloglist/utils/list_helper.js
+++ part-4/section-a/bloglist/utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
-    return 1;
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    let sum = 0
+
+    blogs.forEach(blog => {
+        sum += blog.likes
+    })
+
+    return sum
 }
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
