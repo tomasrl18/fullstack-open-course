ef9db9bb174cff72b3b870255bc13c5074e20443
Tomás R <tomaslopezraigal@gmail.com>
2024-09-30 20:37:25 +0200

    Example section-a: Some changes

--- part-4/section-a/examples/app.js
+++ part-4/section-a/examples/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 const cors = require('cors')
 
-const notesRouter = require('./controllers/notes')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -28,7 +28,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/notes', notesRouter)
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
