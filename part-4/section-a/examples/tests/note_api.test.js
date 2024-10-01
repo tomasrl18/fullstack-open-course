62aa80f4dd6398fba55de4285648a08d0386f993
Tomás R <tomaslopezraigal@gmail.com>
2024-10-01 12:13:50 +0200

    Example section a: Execution of test one by one

--- part-4/section-a/examples/tests/note_api.test.js
+++ part-4/section-a/examples/tests/note_api.test.js
@@ -1,9 +1,33 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+
+const Note = require('../models/note')
+
+const initialNotes = [
+    {
+      content: 'HTML is easy',
+      important: false,
+    },
+    {
+      content: 'Browser can execute only JavaScript',
+      important: true,
+    },
+]
+
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const assert = require('node:assert')
+
 const app = require('../app')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+    await Note.deleteMany({})
+    let noteObject = new Note(initialNotes[0])
+    await noteObject.save()
+    noteObject = new Note(initialNotes[1])
+    await noteObject.save()
+})
 
 test('notes are returned as json', async () => {
   await api
@@ -15,12 +39,12 @@ test('notes are returned as json', async () => {
 test('there are two notes', async () => {
     const response = await api.get('/api/notes')
   
-    assert.strictEqual(response.body.length, 2)
+    assert.strictEqual(response.body.length, initialNotes.length)
 })
-  
+
 test('the first note is about HTTP methods', async () => {
     const response = await api.get('/api/notes')
-
+  
     const contents = response.body.map(e => e.content)
     assert(contents.includes('HTML is easy'))
 })
