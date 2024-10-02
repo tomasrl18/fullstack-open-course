0be60c7a6c0401660aff594186e4a3b8477422f7
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-02 20:42:32 +0200

    Creating users

--- part-4/examples/app.js
+++ part-4/examples/app.js
@@ -8,6 +8,8 @@ const app = express()
 const cors = require('cors')
 
 const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -30,6 +32,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part-4/examples/tests/note_api.test.js
+++ part-4/examples/tests/note_api.test.js
@@ -1,10 +1,11 @@
 const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
+
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+
 const app = require('../app')
 const api = supertest(app)
-
 const helper = require('./test_helper')
 
 const Note = require('../models/note')
