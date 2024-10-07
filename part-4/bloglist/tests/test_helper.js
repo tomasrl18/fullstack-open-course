44226f17b50ade5cdbbcff904413666327819c4a
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-07 19:47:28 +0200

    Done exercise 4.15: Expanding the bloglist step3

--- part-4/bloglist/tests/test_helper.js
+++ part-4/bloglist/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
     {
@@ -40,18 +41,26 @@ const initialBlogs = [
 ]
 
 const nonExistingId = async () => {
-    const blog = new Blog({ title: 'willremovethissoon' })
-    await blog.save()
-    await blog.deleteOne()
+  const blog = new Blog({ title: 'willremovethissoon' })
+  await blog.save()
+  await blog.deleteOne()
 
-    return blog._id.toString()
+  return blog._id.toString()
 }
 
 const blogsInDb = async () => {
-    const blogs = await Blog.find({})
-    return blogs.map(blog => blog.toJSON())
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
 }
 
 module.exports = {
-    initialBlogs, nonExistingId, blogsInDb
+  initialBlogs,
+  nonExistingId,
+  blogsInDb,
+  usersInDb
 }
