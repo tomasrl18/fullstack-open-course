cd67ab0d0df89a03939e7ced28d634ce44208cfd
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-08 12:27:52 +0200

    Done exercise 4.21: Expanding the bloglist step9

--- part-4/bloglist/controllers/blogs.js
+++ part-4/bloglist/controllers/blogs.js
@@ -47,6 +47,22 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(404).json({ error: 'blog not found' })
+  }
+
+  if (blog.user.toString() !== decodedToken.id.toString()) {
+    return response.status(403).json({ error: 'only the creator can delete this blog' })
+  }
+  
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
