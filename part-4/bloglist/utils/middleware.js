5ac9dcf67fd987a171254e38e008937773a2aee7
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-08 12:11:21 +0200

    Done exercise 4.20: Expanding the bloglist step8

--- part-4/bloglist/utils/middleware.js
+++ part-4/bloglist/utils/middleware.js
@@ -14,7 +14,7 @@ const unknownEndpoint = (request, response) => {
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
-
+  
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
@@ -24,12 +24,25 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name ===  'JsonWebTokenError') {
     return response.status(401).json({ error: 'token invalid' })
   }
-
+  
   next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+
+  next()
 }
 
 module.exports = {
     requestLogger,
+    tokenExtractor,
     unknownEndpoint,
     errorHandler
 }
