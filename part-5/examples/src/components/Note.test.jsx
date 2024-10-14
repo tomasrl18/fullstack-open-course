eb1e7e52b0d23c7be4afdad2d192af8652ad03ee
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-14 22:36:13 +0200

    Example section c: Clicking buttons in the tests

--- part-5/examples/src/components/Note.test.jsx
+++ part-5/examples/src/components/Note.test.jsx
@@ -1,18 +1,22 @@
 /* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import Note from './Note'
 
-test('renders content', () => {
+test('renders content', async () => {
   const note = {
     content: 'Component testing is done with react-testing-library',
     important: true
   }
 
-  render(<Note important={note.important} content={note.content}  />)
+  const mockHandler = vi.fn()
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
+  render(<Note important={note.important} content={note.content} toggleImportance={mockHandler} />)
 
-  screen.debug(element)
+  const user = userEvent.setup()
+  const button = screen.getByText('Make not important')
+  await user.click(button)
 
-  expect(element).toBeDefined()
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
