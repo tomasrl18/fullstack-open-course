a8ecbe5774b1984b108abda21c95eb149916ac51
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-14 19:58:35 +0200

    Done exercise 5.10: bloglist frontend step10

--- part-5/bloglist-frontend/src/App.jsx
+++ part-5/bloglist-frontend/src/App.jsx
@@ -153,8 +153,15 @@ const App = () => {
           </Togglable>
           
           {
-            blogs.map(blog =>
-              <Blog key={blog.id} blog={blog} user={user} handleUpdateLikes={handleUpdateLikes} />
+            blogs
+              .sort((a, b) => b.likes - a.likes)
+              .map(blog =>
+                <Blog
+                  key={blog.id}
+                  blog={blog}
+                  user={user}
+                  handleUpdateLikes={handleUpdateLikes}
+                />
             )
           }
         </div>
