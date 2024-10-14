97fa4bcc531fd8a58e2837205443553d8393896d
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-14 22:30:14 +0200

    Example section c: Searching the content inside a component

--- part-5/examples/src/components/Note.test.jsx
+++ part-5/examples/src/components/Note.test.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Note from './Note'
 
 test('renders content', () => {
@@ -8,8 +8,10 @@ test('renders content', () => {
     important: true
   }
 
-  render(<Note important={note.important} content={note.content} />)
+  const { container } = render(<Note important={note.important} content={note.content}  />)
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
-  expect(element).toBeDefined()
+  const div = container.querySelector('.note')
+  expect(div).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  )
 })
