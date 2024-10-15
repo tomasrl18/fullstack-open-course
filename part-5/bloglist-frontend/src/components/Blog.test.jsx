cc72d73f1c20cd35ff55d16b569e7afb33606fa8
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-15 11:07:58 +0200

    Done exercis 5.14: Testing bloglist step2

--- part-5/bloglist-frontend/src/components/Blog.test.jsx
+++ part-5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,32 +1,53 @@
 /* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import Blog from './Blog'
 
-test('renders content', async () => {
-  const blog = {
-    title: 'A new blog',
-    author: 'Someone',
-    url: 'http://yeah-i-think-its-ok.com',
-    likes: 143,
-    user: '67041e52aec3bb1cec1fb6fa'
-  }
+describe('blog', () => {
+    let container;
+    
+    const blog = {
+        title: 'A new blog',
+        author: 'Someone',
+        url: 'http://yeah-i-think-its-ok.com',
+        likes: 143,
+        user: '67041e52aec3bb1cec1fb6fa'
+    }
 
-  const user = {
-    name: 'Someone',
-    username: 'someone1992'
-  }
+    const user = {
+        name: 'Someone',
+        username: 'someone1992'
+    }
 
-  const mockHandler = vi.fn()
+    const mockHandler = vi.fn()
 
-  render(<Blog blog={blog} user={user} handleUpdateLikes={mockHandler} handleDeleteBlog={mockHandler} />)
+    beforeEach(() => {
+        container = render(
+            <Blog blog={blog} user={user} handleUpdateLikes={mockHandler} handleDeleteBlog={mockHandler} />
+        ).container
+    })
+    
+    test('renders content', async () => {
+      const titleElement = screen.getByText('A new blog')
+      expect(titleElement).toBeDefined()
+    
+      const urlElement = screen.queryByText('http://yeah-i-think-its-ok.com')
+      const likesElement = screen.queryByText('likes 143')
+    
+      expect(urlElement).toBeNull()
+      expect(likesElement).toBeNull()
+    })
+    
+    test('showing details when click', async () => {
+        const clickerUser = userEvent.setup()
+        const button = screen.getByText('View details')
+        await clickerUser.click(button)
+    
+        const urlElement = screen.getByText('http://yeah-i-think-its-ok.com')
+        const likesElement = screen.getByText('143')
 
-  const titleElement = screen.getByText('A new blog')
-  expect(titleElement).toBeDefined()
-
-  const urlElement = screen.queryByText('http://yeah-i-think-its-ok.com')
-  const likesElement = screen.queryByText('likes 143')
-
-  expect(urlElement).toBeNull()
-  expect(likesElement).toBeNull()
+        expect(urlElement).toBeDefined()
+        expect(likesElement).toBeDefined()
+    })
 })
