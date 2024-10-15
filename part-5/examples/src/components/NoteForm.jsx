da7b87d2ef0a8df6f0000a71effbc9ef844c569e
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-15 12:38:21 +0200

    Example section d: Testing the form to add notes

--- part-5/examples/src/components/NoteForm.jsx
+++ part-5/examples/src/components/NoteForm.jsx
@@ -25,6 +25,7 @@ const NoteForm = ({ createNote }) => {
             value={newNote}
             onChange={event => setNewNote(event.target.value)}
             id='note-input'
+            data-testid='new-note'
           />
         </div>
         <button type="submit" style={{ marginTop: '0.5rem', marginBottom: '1rem' }}>Save</button>
--- part-5/examples/tests/note_app.spec.js
+++ part-5/examples/tests/note_app.spec.js
@@ -5,20 +5,38 @@ test.describe('Note app', () => {
         await page.goto('http://localhost:5173')
     })
     
-    test.skip('front page can be opened', async ({ page }) => {
+    test.skip('Front page can be opened', async ({ page }) => {
       const locator = await page.getByText('Notes')
       await expect(locator).toBeVisible()
       await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
     })
 
-    test('login form can be opened', async ({ page }) => {
+    test('user can log in', async ({ page }) => {
         await page.getByRole('button', { name: 'Login' }).click()
-
+        
         await page.getByTestId('username').fill('tomasrl')
         await page.getByTestId('password').fill('123456789')
 
         await page.getByRole('button', { name: 'Login' }).click()
-    
+
         await expect(page.getByText('Tomas logged in')).toBeVisible()
     })
+    
+    test.describe('When logged in', () => {
+        test.beforeEach(async ({ page }) => {
+            await page.getByRole('button', { name: 'Login' }).click()
+
+            await page.getByTestId('username').fill('tomasrl')
+            await page.getByTestId('password').fill('123456789')
+
+            await page.getByRole('button', { name: 'Login' }).click()
+        })
+
+        test('A new note can be created', async ({ page }) => {
+            await page.getByTestId('new-note').fill('A note created by playwright')
+            await page.getByRole('button', { name: 'Save' }).click()
+            await expect(page.getByText('A note created by playwright')).toBeVisible()
+        })
+    })
+
 })
