4f22883121b83af6951e07cd73143f86eadf7c8f
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-17 18:18:50 +0200

    Example section a: Action creators

--- part-6/examples/src/main.jsx
+++ part-6/examples/src/main.jsx
@@ -7,26 +7,34 @@ const store = createStore(noteReducer);
 
 const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
 const App = () => {
   const addNote = (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = "";
-    store.dispatch({
-      type: "NEW_NOTE",
-      payload: {
-        content,
-        important: false,
-        id: generateId(),
-      },
-    });
+    store.dispatch(createNote(content))
   };
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: "TOGGLE_IMPORTANCE",
-      payload: { id },
-    });
+    store.dispatch(toggleImportanceOf(id))
   };
 
   return (
@@ -38,7 +46,7 @@ const App = () => {
       <ul>
         {store.getState().map((note) => (
           <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
+            {note.content} <strong>{note.important ? "important" : "not important"}</strong>
           </li>
         ))}
       </ul>
