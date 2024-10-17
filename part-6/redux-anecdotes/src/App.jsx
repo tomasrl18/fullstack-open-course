d3b34c3d393b798bcf27a19697368cefbb270ce7
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-17 19:40:24 +0200

    Done exercise 6.5: Anecdotes step 3

--- part-6/redux-anecdotes/src/App.jsx
+++ part-6/redux-anecdotes/src/App.jsx
@@ -17,16 +17,19 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(increaseVotes(anecdote.id))}>vote</button>
-          </div>
-        </div>
+      {
+        anecdotes
+          .sort((a, b) => b.votes - a.votes)
+          .map(anecdote =>
+            <div key={anecdote.id}>
+              <div>
+                {anecdote.content}
+              </div>
+              <div>
+                has {anecdote.votes}
+                <button onClick={() => dispatch(increaseVotes(anecdote.id))}>vote</button>
+              </div>
+            </div>
       )}
       <h2>create new</h2>
       <form onSubmit={addAnecdote}>
