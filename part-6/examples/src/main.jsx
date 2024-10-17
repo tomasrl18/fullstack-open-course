34d523b4e47f23ebb6b1348d093a8d72690517ac
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-17 20:50:46 +0200

    Example section b: Finishing filters

--- part-6/examples/src/main.jsx
+++ part-6/examples/src/main.jsx
@@ -16,21 +16,8 @@ const reducer = combineReducers({
 
 const store = createStore(reducer);
 
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <div />
-  </Provider>
-)
-
-/* ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
-); */
+)
