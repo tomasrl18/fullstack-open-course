c0e2e411de28779b75982e0fbf8bbe6d803101cf
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-17 18:42:12 +0200

    Example section a: Forwarding Redux-Store to several components

--- part-6/examples/src/main.jsx
+++ part-6/examples/src/main.jsx
@@ -1,64 +1,16 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { legacy_createStore as createStore } from "redux";
+
+import { Provider } from "react-redux";
+
+import App from "./App";
 import noteReducer from "./reducers/noteReducer";
 
 const store = createStore(noteReducer);
 
-const generateId = () => Number((Math.random() * 1000000).toFixed(0))
-
-const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    payload: {
-      content,
-      important: false,
-      id: generateId()
-    }
-  }
-}
-
-const toggleImportanceOf = (id) => {
-  return {
-    type: 'TOGGLE_IMPORTANCE',
-    payload: { id }
-  }
-}
-
-const App = () => {
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value = "";
-    store.dispatch(createNote(content))
-  };
-
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id))
-  };
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map((note) => (
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? "important" : "not important"}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const renderApp = () => {
-  root.render(<App />);
-};
-
-renderApp();
-store.subscribe(renderApp);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
