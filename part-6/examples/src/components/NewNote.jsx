73e5b9476711484b12b915e20a0abe868e4580c7
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-21 20:48:27 +0200

    Example section c: Async actions and Redux Thunk

--- part-6/examples/src/components/NewNote.jsx
+++ part-6/examples/src/components/NewNote.jsx
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux'
-import { createNote } from '../reducers/noteReducer'
 
-import noteService from '../services/notes'
+import { createNote } from '../reducers/noteReducer'
 
 const NewNote = () => {
     const dispatch = useDispatch()
@@ -11,8 +10,7 @@ const NewNote = () => {
         const content = event.target.note.value
         event.target.note.value = ''
 
-        const newNote = await noteService.createNew(content)
-        dispatch(createNote(newNote))
+        dispatch(createNote(content))
     }
 
     const formStyle = {
--- part-6/examples/src/reducers/noteReducer.js
+++ part-6/examples/src/reducers/noteReducer.js
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+import noteService from '../services/notes'
+
 const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
@@ -30,5 +32,20 @@ const noteSlice = createSlice({
   },
 })
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = content => {
+  return async dispatch => {
+    const newNote = await noteService.createNew(content)
+    dispatch(appendNote(newNote))
+  }
+}
+
 export default noteSlice.reducer
