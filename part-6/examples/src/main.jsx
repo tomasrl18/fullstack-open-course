450c55dcdc5ab3443b602f7aa1ac7b1208af3194
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-21 20:13:21 +0200

    Example section c: Obtain data from the backend

--- part-6/examples/src/main.jsx
+++ part-6/examples/src/main.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react"
+import ReactDOM from "react-dom/client"
 import { configureStore } from '@reduxjs/toolkit'
 
-import { Provider } from "react-redux";
+import { Provider } from "react-redux"
 
-import App from "./App";
+import App from "./App"
 
-import noteReducer from "./reducers/noteReducer";
+import noteService from './services/notes'
+
+import noteReducer, { setNotes } from "./reducers/noteReducer"
 import filterReducer from './reducers/filterReducer'
 
 const store = configureStore({
@@ -14,7 +16,11 @@ const store = configureStore({
     notes: noteReducer,
     filter: filterReducer
   }
-});
+})
+
+noteService.getAll().then(notes =>
+  store.dispatch(setNotes(notes))
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
