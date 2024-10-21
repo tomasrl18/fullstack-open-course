83988393467fe798e23083dfd582e5196884a42f
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-21 19:09:32 +0200

    Done exercise 6.10: Better anecdotes step8

--- part-6/redux-anecdotes/src/main.jsx
+++ part-6/redux-anecdotes/src/main.jsx
@@ -1,19 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
+import store from './store'
 
 import { Provider } from 'react-redux'
 
 import App from './App'
-
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const store = configureStore({
-  reducer: {
-    anecdotes: anecdoteReducer,
-    filter: filterReducer
-  }
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
