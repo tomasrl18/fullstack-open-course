13530a6edb3d6e4ced503af3127281c7b681966d
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-21 18:25:39 +0200

    Redux toolkit

--- part-6/examples/src/main.jsx
+++ part-6/examples/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { legacy_createStore as createStore, combineReducers } from "redux";
+import { configureStore } from '@reduxjs/toolkit'
 
 import { Provider } from "react-redux";
 
@@ -9,12 +9,12 @@ import App from "./App";
 import noteReducer from "./reducers/noteReducer";
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer);
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
