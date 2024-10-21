2b2851f918d4ce859a18affa81983b69741d1c7e
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-21 22:29:57 +0200

    Done exercise 6.18: Anecdotes and backend step5

--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -6,17 +6,11 @@ const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
-    increaseVotes(state, action) {
-      const id = action.payload
-
-      const anecdoteToChange = state.find(n => n.id === id);
-
-      const changedAnecdote = {
-        ...anecdoteToChange,
-        votes: anecdoteToChange.votes + 1,
-      };
-
-      return state.map(anecdote => (anecdote.id !== id ? anecdote : changedAnecdote));
+    updateAnecdote(state, action) {
+      const updatedAnecdote = action.payload
+      return state.map(anecdote =>
+        anecdote.id !== updatedAnecdote.id ? anecdote : updatedAnecdote
+      )
     },
     appendAnecdote(state, action) {
       state.push(action.payload)
@@ -27,7 +21,7 @@ const anecdoteSlice = createSlice({
   }
 })
 
-export const { increaseVotes, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+export const { updateAnecdote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
 
 export const initializeAnecdotes = () => {
   return async dispatch => {
@@ -40,6 +34,21 @@ export const newAnecdote = content => {
   return async dispatch => {
     const newAnecdote = await anecdoteService.createNew(content)
     dispatch(appendAnecdote(newAnecdote))
+  }
+}
+
+export const increaseVotes = (id) => {
+  return async (dispatch, getState) => {
+    const state = getState().anecdotes
+    const anecdoteToUpdate = state.find(anecdote => anecdote.id === id)
+
+    const updatedAnecdote = {
+      ...anecdoteToUpdate,
+      votes: anecdoteToUpdate.votes + 1
+    }
+
+    const savedAnecdote = await anecdoteService.update(id, updatedAnecdote)
+    dispatch(updateAnecdote(savedAnecdote))
   }
 }
 
