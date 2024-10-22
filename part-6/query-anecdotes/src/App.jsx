59fd2844ee20dbee175b59ef09ae351fe7569bad
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-22 12:09:30 +0200

    Done exercise 6.21: Query anecdotes step2

--- part-6/query-anecdotes/src/App.jsx
+++ part-6/query-anecdotes/src/App.jsx
@@ -1,8 +1,8 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, createAnecdote } from './requests'
 
 const App = () => {
   const handleVote = (anecdote) => {
--- part-6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part-6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,18 +1,34 @@
-const AnecdoteForm = () => {
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
 
-  const onCreate = (event) => {
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    }
+  })
+
+  console.log(newAnecdoteMutation);
+
+  const addAnecdote = async (event) => {
     event.preventDefault()
+
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+
+    newAnecdoteMutation.mutate({ content, vottes: 0 })
+  }
 
   return (
     <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
+      <h3>Create a new anecdote</h3>
+      <form onSubmit={addAnecdote}>
         <input name='anecdote' />
-        <button type="submit">create</button>
+        <button type="submit">Create</button>
       </form>
     </div>
   )
