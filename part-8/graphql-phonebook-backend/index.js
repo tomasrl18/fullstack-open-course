c68f1477e7f6d72c6abeceff565db6ac6e52368c
Tomás R. <tomaslopezraigal@gmail.com>
2024-10-29 12:30:46 +0100

    Example section a: Mutations

--- part-8/graphql-phonebook-backend/index.js
+++ part-8/graphql-phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let persons = [
   {
@@ -25,11 +26,15 @@ let persons = [
 ]
 
 const typeDefs = `
+  type Address {
+    street: String!
+    city: String! 
+  }
+
   type Person {
     name: String!
     phone: String
-    street: String!
-    city: String! 
+    address: Address! 
     id: ID!
   }
 
@@ -37,6 +42,15 @@ const typeDefs = `
     personCount: Int!
     allPersons: [Person!]!
     findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
   }
 `
 
@@ -46,6 +60,21 @@ const resolvers = {
     allPersons: () => persons,
     findPerson: (root, args) =>
       persons.find(p => p.name === args.name)
+  },
+  Person: {
+    address: (root) => {
+      return { 
+        street: root.street,
+        city: root.city
+      }
+    }
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      const person = { ...args, id: uuid() }
+      persons = persons.concat(person)
+      return person
+    }
   }
 }
 
