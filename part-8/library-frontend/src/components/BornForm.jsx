67ab7fe906486f383ad7346b2b461eeaeb5a8541
Tomás R. <tomaslopezraigal@gmail.com>
2024-11-02 14:37:55 +0100

    Done exercise 8.11: Author born date advanced

--- part-8/library-frontend/src/components/BornForm.jsx
+++ part-8/library-frontend/src/components/BornForm.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
-import { useMutation } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 
 import { ALL_AUTHORS, EDIT_BORN } from '../queries'
+
+import Select from 'react-select';
 
 const BornForm = () => {
     const [name, setName] = useState('')
     const [born, setBorn] = useState('')
 
+    const { loading, data } = useQuery(ALL_AUTHORS)
+
     const [ changeBorn ] = useMutation(EDIT_BORN, {
         refetchQueries: [ { query: ALL_AUTHORS } ],
     })
+
+    if (loading) return <div>Loading...</div>
+
+    const authorOptions = data.allAuthors.map(author => ({
+        value: author.name,
+        label: author.name
+    }))
 
     const submit = (event) => {
         event.preventDefault()
@@ -25,14 +36,16 @@ const BornForm = () => {
             <h2>Change born date</h2>
 
             <form onSubmit={submit}>
-                <div>
-                name <input
-                    value={name}
-                    onChange={({ target }) => setName(target.value)}
+                <label>Author</label>
+                <Select
+                    value={authorOptions.find(option => option.value === name)}
+                    onChange={(selectedOption) => setName(selectedOption.value)}
+                    options={authorOptions}
+                    placeholder="Select author"
                 />
-                </div>
+                
                 <div>
-                born <input
+                Born <input
                     type='number'
                     value={born}
                     onChange={({ target }) => setBorn(target.value)}
