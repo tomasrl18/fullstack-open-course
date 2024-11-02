59cd5370559d14aea89c7330e113b0a1cf1cba27
Tomás R. <tomaslopezraigal@gmail.com>
2024-11-02 12:50:58 +0100

    Example section b: Editing the phone number

--- part-8/graphql-phonebook-frontend/src/App.jsx
+++ part-8/graphql-phonebook-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { useQuery } from '@apollo/client';
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm';
 import Notify from './components/Notify'
+import PhoneForm from './components/PhoneForm';
 
 import { ALL_PERSONS } from './queries';
 
@@ -28,6 +29,7 @@ const App = () => {
       <Notify errorMessage={errorMessage} />
       <Persons persons = {result.data.allPersons}/>
       <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
   )
 }
