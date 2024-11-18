34971c732eded454ef4ebae43bbdcd1100d10032
Tomás R. <tomaslopezraigal@gmail.com>
2024-11-18 19:18:09 +0100

    Done exercesie 9.1: Body mass index

---
+++ part-9/exercises/bmiCalculator.ts
@@ -0,0 +1,20 @@
+const calculateBmi = (mass: number, height: number) => {
+    let heightMeter = height / 100;
+    let heightSquare = heightMeter * heightMeter;
+    
+    let calculation: number = (mass / heightSquare);
+
+    if (calculation < 18.5) {
+        return 'Low weight'
+    }
+
+    if (calculation >= 18.5 && calculation <= 24.99) {
+        return 'Normal (healthy weight)'
+    }
+
+    if (calculation >= 25) {
+        return 'Overweight'
+    }
+}
+
+console.log(calculateBmi(74, 180));
