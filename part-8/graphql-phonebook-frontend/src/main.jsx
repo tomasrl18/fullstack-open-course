18550a7d8590569081720be8f5db44fb7417d248
Tomás R. <tomaslopezraigal@gmail.com>
2024-11-18 18:24:57 +0100

    Some changes

--- part-8/graphql-phonebook-frontend/src/main.jsx
+++ part-8/graphql-phonebook-frontend/src/main.jsx
@@ -1,8 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  split
+} from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
+
+import { getMainDefinition } from '@apollo/client/utilities'
+import { WebSocketLink } from '@apollo/client/link/ws'
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('phonenumbers-user-token')
@@ -14,13 +23,32 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-const httpLink = createHttpLink({
+const httpLink = HttpLink({
   uri: 'http://localhost:4000',
 })
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true
+  }
+})
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink),
+)
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink)
+  link: splitLink
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
