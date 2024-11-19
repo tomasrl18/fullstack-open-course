085546975d70979232c24e988a659818f8e87778
Tomás R. <tomaslopezraigal@gmail.com>
2024-11-19 21:02:05 +0100

    Example section b: The any horrors

--- part-9/examples/calculator.ts
+++ part-9/examples/calculator.ts
@@ -1,7 +1,7 @@
 type Operation = 'multiply' | 'add' | 'divide';
 type Result = string | number
 
-const calculator = (a: number, b: number, op: Operation): Result => {
+export const calculator = (a: number, b: number, op: Operation): Result => {
     switch (op) {
         case 'multiply':
             return a * b;
